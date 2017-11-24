95ab15aadc50df69b01d8970019fd518201d385d
Alexander Gubanov <agubanov@avito.ru>
2017-11-25 01:36:37 +0300

    scripts s

---
+++ opencv.py
@@ -0,0 +1,15 @@
+import os
+from typing import Optional, Tuple
+
+import cv2
+
+_cascade = cv2.CascadeClassifier('%s/haarcascade_frontalface_default.xml' % os.path.dirname(os.path.realpath(__file__)))
+
+
+def detect_face(path: str) -> Optional[Tuple[int]]:
+    img = cv2.imread(path, 0)
+    faces = _cascade.detectMultiScale(img, 1.1, 10)
+    if len(faces):
+        return sorted(faces, key=lambda f: f[2] * f[3])[0]
+
+    return None
