bb9eb69542e44f1e87a6b5652154d567490fe192
Alexander Gubanov <agubanov@avito.ru>
2017-11-25 01:43:45 +0300

    opencv

--- opencv.py
+++ opencv.py
@@ -7,6 +7,9 @@ _cascade = cv2.CascadeClassifier('%s/haarcascade_frontalface_default.xml' % os.p
 
 
 def detect_face(path: str) -> Optional[Tuple[int]]:
+    """
+    :return: x, y, w, h
+    """
     img = cv2.imread(path, 0)
     faces = _cascade.detectMultiScale(img, 1.1, 10)
     if len(faces):
